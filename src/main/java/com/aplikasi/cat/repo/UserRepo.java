f5b06b73b3ddf2f356723bec7628135f20b9ed52
Damar <damarsepti627@gmail.com>
2022-06-18 22:13:24 +0700

    Fix UserRepo

--- src/main/java/com/aplikasi/cat/repo/UserRepo.java
+++ src/main/java/com/aplikasi/cat/repo/UserRepo.java
@@ -9,15 +9,15 @@ import org.springframework.data.repository.query.Param;
 import com.aplikasi.cat.dto.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-	@Query(value = "SELECT * FROM User WHERE username = :username AND password = :password", nativeQuery = true)
+	@Query(value = "SELECT * FROM user_app WHERE username = :username AND password = :password", nativeQuery = true)
 	User findUserByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 	
-	@Query(value = "SELECT * FROM User WHERE role <> 1 ORDER BY role, username, nama_lengkap", nativeQuery = true)
+	@Query(value = "SELECT * FROM user_app WHERE role <> 1 ORDER BY role, username, nama_lengkap", nativeQuery = true)
 	List<User> findAllUser();
 	
-	@Query(value = "SELECT * FROM User WHERE id = :id", nativeQuery = true)
+	@Query(value = "SELECT * FROM user_app WHERE id = :id", nativeQuery = true)
 	User findUserById(@Param("id") Long id);
 	
-	@Query(value = "SELECT * FROM User WHERE username = :username", nativeQuery = true)
+	@Query(value = "SELECT * FROM user_app WHERE username = :username", nativeQuery = true)
 	User findUserByUsername(@Param("username") String username);
 }
